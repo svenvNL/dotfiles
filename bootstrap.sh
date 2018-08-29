dc2fa2e8e582786d241bc8318558c1f6a2883f30
Sven van der Vlist <svenvdvlist@live.nl>
2018-08-29 21:22:45 +0200

    Add VSCode settings

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,3 +5,4 @@ ln -fs "$PWD/.bash_profile" ~/.bash_profile
 ln -fs "$PWD/.bash_prompt" ~/.bash_prompt
 ln -fs "$PWD/.bashrc" ~/.bashrc
 ln -fs "$PWD/.gitconfig" ~/.gitconfig
+ln -fs "$PWD/Code/settings.json" ~/.config/Code/User/settings.json
