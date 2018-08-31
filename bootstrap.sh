e7e433194a3e6fb14297875e1e83212a1a0a4998
Sven van der Vlist <svenvdvlist@live.nl>
2018-08-31 16:16:22 +0200

    remove non-working code

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,29 +1,17 @@
 #!/bin/bash
 
-# Catch and log errors
-trap uncaughtError ERR
-function uncaughtError {
-    echo -e "\n❌  Error\n"
-    echo -e "${ERR}"
-    exit $?
-}
-
-function initLogFile() {
-    ERROR_LOG="${HOME}/.dotfile-install-err.log"
-}
-
 function setupDirectories() {
     PROJECTS_DIR="${HOME}/Projects"
     DOTFILES_DIR="${PROJECTS_DIR}/dotfiles"
     
-    echo -e "📂  Setting up directories..."
+    echo -e "Setting up directories..."
     echo -e "\tProjects:\t${PROJECTS_DIR}"
     
     mkdir -p ${PROJECTS_DIR}
 }
 
 function setupXClip() {
-    sudo dnf -y install xclip &> ${ERROR_LOG}
+    sudo dnf -y install xclip
 }
 
 function setupSSHKeys() {
@@ -31,18 +19,13 @@ function setupSSHKeys() {
 }
 
 function cloneRepo() {
-    echo -e "\n🖥  Cloning dotfiles..."
-    git clone git@github.com:svenvNL/dotfiles.git ${DOTFILES_DIR} &> ${ERROR_LOG}
-    echo -e "\n\t✅  Done"
+    echo -e "\nCloning dotfiles..."
+    git clone git@github.com:svenvNL/dotfiles.git ${DOTFILES_DIR}
 }
 
 function runSetup() {
     bash "${DOTFILES_DIR}/setup.sh"
 }
-
-echo -e "👢  Bootstrapping Dotfiles...\n"
-
-initLogFile
 
 setupDirectories
 
--- setup.sh
+++ setup.sh
@@ -1,65 +1,52 @@
 #!/bin/bash
 
-# Catch and log errors
-trap uncaughtError ERR
-function uncaughtError {
-    echo -e "\n❌  Error\n"
-    echo -e "${ERR}"
-    exit $?
-}
-
-function initLogFile() {
-    ERROR_LOG="${HOME}/.dotfile-install-err.log"
-}
-
 function setupSoftware() {
-    echo -e "\n📦  Installing software..."
-    sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc &> ${ERROR_LOG}
+    echo -e "\nInstalling software..."
+    sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
     
-    wget -q https://packages.microsoft.com/config/fedora/27/prod.repo &> ${ERROR_LOG}
+    wget -q https://packages.microsoft.com/config/fedora/27/prod.repo
     sudo mv prod.repo /etc/yum.repos.d/microsoft-prod.repo
-    sudo chown root:root /etc/yum.repos.d/microsoft-prod.repo &> ${ERROR_LOG}
+    sudo chown root:root /etc/yum.repos.d/microsoft-prod.repo
     
     sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
     
-    sudo dnf copr -y enable evana/fira-code-fonts &> ${ERROR_LOG}
-    sudo dnf copr -y enable dirkdavidis/papirus-icon-theme &> ${ERROR_LOG}
+    sudo dnf copr -y enable evana/fira-code-fonts
+    sudo dnf copr -y enable dirkdavidis/papirus-icon-theme
     
-    sudo dnf install fedora-workstation-repositories &> ${ERROR_LOG}
-    sudo dnf config-manager --set-enabled google-chrome &> ${ERROR_LOG}
+    sudo dnf install fedora-workstation-repositories
+    sudo dnf config-manager --set-enabled google-chrome
     
-    sudo dnf -y upgrade &> ${ERROR_LOG}
+    sudo dnf -y upgrade
     
-    sudo dnf -y install snapd &> ${ERROR_LOG}
+    sudo dnf -y install snapd
     sudo ln -fs /var/lib/snapd/snap /snap
     
-    sudo dnf -y install code &> ${ERROR_LOG}
-    sudo dnf -y install dotnet-sdk-2.1 &> ${ERROR_LOG}
-    sudo dnf -y install nodejs &> ${ERROR_LOG}
-    sudo dnf -y install bijiben &> ${ERROR_LOG}
-    sudo dnf -y install arc-theme &> ${ERROR_LOG}
-    sudo dnf -y install gnome-tweaks &> ${ERROR_LOG}
-    sudo dnf -y install fira-code-fonts &> ${ERROR_LOG}
-    sudo dnf -y install papirus-icon-theme &> ${ERROR_LOG}
-    sudo dnf -y install google-chrome-stable &> ${ERROR_LOG}
+    sudo dnf -y install code
+    sudo dnf -y install dotnet-sdk-2.1
+    sudo dnf -y install nodejs
+    sudo dnf -y install bijiben
+    sudo dnf -y install arc-theme
+    sudo dnf -y install gnome-tweaks
+    sudo dnf -y install fira-code-fonts
+    sudo dnf -y install papirus-icon-theme
+    sudo dnf -y install google-chrome-stable
     
-    sudo snap install spotify &> ${ERROR_LOG}
+    sudo snap install spotify
     
     # shotwell
     # jetbrains toolbox
     # jetbrains rider
     # proprietary codecs
     # fat32
-    echo -e "\n\t✅  Done"
 }
 
 function setupNodePackages() {
-    echo -e "\n📦  Installing global node packages..." &> ${ERROR_LOG}
+    echo -e "\nInstalling global node packages..."
     sudo npm i -g --unsafe-perm bash-language-server
 }
 
 function setupSoftLinks() {
-    echo -e "\n🔗  Setting up soft links"
+    echo -e "\nSetting up soft links"
     
     ln -fs "$PWD/.aliases" ~/.aliases
     ln -fs "$PWD/.bash_profile" ~/.bash_profile
@@ -70,12 +57,10 @@ function setupSoftLinks() {
     ln -fs "$PWD/.gitconfig" ~/.gitconfig
     ln -fs "$PWD/.inputrc" ~/.inputrc
     ln -fs "$PWD/Code/settings.json" ~/.config/Code/User/settings.json;
-    
-    echo -e "\n\t✅  Done"
 }
 
 function setupGit() {
-    echo -e "\n🖥️  Setting up Git..."
+    echo -e "\nSetting up Git..."
     
     read -p "Email": GIT_EMAIL
     read -p "Name": GIT_NAME
@@ -84,11 +69,7 @@ function setupGit() {
     GIT_CONFIG+="email = ${GIT_EMAIL}\n"
     GIT_CONFIG+="name = ${GIT_NAME}"
     echo -e ${GIT_CONFIG} > "${HOME}/.gitconfig.local"
-    
-    echo -e "\n\t✅  Done"
 }
-
-initLogFile
 
 setupSoftware
 
@@ -97,5 +78,3 @@ setupNodePackages
 setupSoftLinks
 
 setupGit
-
-echo -e "\n🎉  Finished, reboot to complete.\n"
